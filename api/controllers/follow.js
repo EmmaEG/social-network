0adf5ade244a11509207e160052e2f709250b960
Emmanuel Granara <emanuelgranara@hotmail.com>
2020-10-13 09:40:27 -0300

    usuarios no paginados

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -65,12 +65,63 @@ function getFollowingUsers(req, res) {
     });
 }
 
+function getFollowedUsers(req, res) {
+    var userId = req.user.sub; //usuario logueado
+
+    if (req.params.id) {
+        userId = req.params.id;
+    }
+
+    var page = 1; //por defecto la pagina sea 1 
+    //si nos llega una pagina por la url
+    if (req.params.page) {
+        page = req.params.page;
+    } else {
+        page = req.params.id;
+    }
+
+    var itemsPerPage = 4; 
+
+    //buscamos los objetos que esten siguiendo al usuario logueado
+    Follow.find({followed: userId}).populate('user').paginate(page, itemsPerPage, (err, follows, total) => {
+        if (err) return res.status(500).send({message: 'Error en el servidor'});
+
+        if (!follows) return res.status(404).send({message: 'No te siguen usuarios'});
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage), //ceil es para redondear
+            follows
+        });
+    });
+}
+
+//listado de usuarios no paginados
+function getMyFollows(req, res) {
+    var userId = req.user.sub;
+
+    var find = Follow.find({user: userId});
+
+    if(req.params.followed) {
+        find = Follow.find({followed: userId});
+    }
+
+    find.populate('user followed').exec((err, follows) => {
+        if (err) return res.status(500).send({message: 'Error en el servidor'});
+
+        if (!follows) return res.status(404).send({message: 'No sigues a ningun usuario'});
+
+        return res.status(200).send({follows});
+    });
+}
 
 
 module.exports = {
     saveFollow,
     deleteFollow,
-    getFollowingUsers
+    getFollowingUsers,
+    getFollowedUsers,
+    getMyFollows
 }
 
 // * en la apropiedad user del objeto req adjunté a la hora de hacerr la autenticación un objeto con toodo
