c040cbb3133d2e34e329f3945e8650aba5cf6b14
Emmanuel Granara <emanuelgranara@hotmail.com>
2020-12-19 11:38:25 -0300

    banner-beinvenida

--- front/src/app/components/home/home.component.ts
+++ front/src/app/components/home/home.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [UserService]
 })
 export class HomeComponent implements OnInit {
     public title: string;
+    public identity;
 
-  constructor() {
+  constructor(
+    private userService: UserService,
+  ) {
     this.title = 'La mejor comunidad de lectores <br> en español';
   }
 
   ngOnInit(): void {
+    this.identity = this.userService.getIdentity();
+
+  }
+
+  ngDoCheck() {
+    this.identity = this.userService.getIdentity();
   }
 
 }
